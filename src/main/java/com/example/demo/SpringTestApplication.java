da59d5f0102436cb6a182013379cba140f24700f
angeluk00700 <angeluk00700@gmail.com>
2017-06-30 17:31:25 +0900

    화면출력

--- src/main/java/com/example/demo/SpringTestApplication.java
+++ src/main/java/com/example/demo/SpringTestApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.web.WelcomeController;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=WelcomeController.class)
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
