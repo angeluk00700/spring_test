92d8e7d23de47db97ccaeff2fae70c2e3facfc9a
angeluk00700 <angeluk00700@gmail.com>
2017-06-30 17:49:59 +0900

    thymleaf 최초 이용

--- src/main/java/com/example/web/WelcomeController.java
+++ src/main/java/com/example/web/WelcomeController.java
@@ -1,15 +1,20 @@
 package com.example.web;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@EnableAutoConfiguration
 @RequestMapping("test")
 public class WelcomeController {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String welcome() {
+	public String welcome(String name, Model model) {
+		System.out.println("name : " + name);
+		model.addAttribute("name", name);
 		return "test/hello";
 	}
 }
